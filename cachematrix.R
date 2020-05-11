e1a614699771e4a68c6d179c3d43b543b053b479
Megha1091 <63605096+Megha1091@users.noreply.github.com>
2020-05-11 10:16:47 +0200

    Test Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,13 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <- y
+    inv <- NULL
+  }
+  get<-function() x
+  setinverse <- 
 }
 
 
